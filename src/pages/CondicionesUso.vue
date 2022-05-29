f0a2a129c89d8d9d2a36cf2f1e18fec9a78ebde9
manuel <manuel.garcia@intelligenia.com>
2022-05-29 13:13:44 +0200

    paginas legales hechas

--- src/pages/CondicionesUso.vue
+++ src/pages/CondicionesUso.vue
@@ -1,5 +1,116 @@
 <template>
-  <q-page class="auth-container"> condiciones uso </q-page>
+  <q-page class="auth-container">
+    <q-header class="m-header"><span>E-Schools</span></q-header>
+    <div class="contenedor">
+      <div class="btn-container">
+        <div class="btn-addnew" @click="goBack">Volver atrás</div>
+      </div>
+      <div class="contenido">
+        <div class="title">Condiciones de uso</div>
+        <div class="text">
+          <h5>Quiénes somos</h5>
+          La presente página web (E-Schools), tiene dirección URL y está
+          identificada con los datos que puedes encontrar en la Política de
+          Privacidad
+          <span class="ver-otro" @click="goOtro"
+            >(ver política de privacidad)</span
+          >.
+          <h5>Qué regulan las Condiciones</h5>
+          Las presentes condiciones de uso regulan el acceso y utilización de
+          los servicios de la sociedad de la información que ponemos a vuestra
+          disposición como usuarios de la red Internet a través del Sitio Web
+          (en cualquiera de sus modalidades, aplicaciones, etc. y para cualquier
+          tipo de dispositivos -fijos o móviles- a través de los cuales se
+          acceda al Sitio Web), así como, en general, la relación entre vosotros
+          Usuarios y el Sitio Web, a salvo de otras comunicaciones, condiciones
+          e instrucciones particulares o específicas, que en cada caso se pongan
+          en vuestro conocimiento.<br /><br />
+          Vuestro acceso o mera utilización del Sitio Web implica la adhesión
+          como Usuarios a las Condiciones publicadas en cada momento en el Sitio
+          Web y que estarán disponibles para los Usuarios. En consecuencia,
+          debes leer atentamente estas Condiciones. En cualquier momento, podrás
+          guardar o imprimir las Condiciones y aconsejamos que lo hagas.
+          <h5>
+            Información previa para el acceso a los servicios del Sitio Web
+          </h5>
+          Antes de usar los servicios del Sitio Web o de registraros, los
+          Usuarios asumís expresamente que sois personas capaces de discernir,
+          que habéis entrado al Sitio Web por vuestra propia voluntad y que no
+          habéis sido coaccionados para ello, y que la selección de los
+          servicios que llevéis a cabo la hacéis por vuestra sola y única cuenta
+          y riesgo, aceptando expresamente estas Condiciones y la
+          <span class="ver-otro" @click="goOtro">Política de Privacidad</span>,
+          así como aquellos otros avisos e instrucciones, comunicaciones, o
+          condiciones particulares, que en cada caso se ponga en vuestro
+          conocimiento.<br /><br />
+          En relación con las condiciones particulares que regulen otros
+          servicios del Sitio Web, estas Condiciones completan lo previsto en
+          esas condiciones particulares en la medida que no se opongan a las
+          mismas.<br /><br />
+          Debes saber que las comunicaciones a través de redes abiertas están
+          expuestas a amenazas que hacen que no sean seguras. La Sociedad adopta
+          todas las medidas de seguridad adecuadas para un correcto
+          funcionamiento del Sitio Web. No obstante, el Usuario debe adoptar
+          todas las medidas técnicas que le correspondan para controlar
+          razonablemente estas amenazas (tener sistemas actualizados de
+          detección de software malicioso, tales como virus, troyanos, etc.,
+          tener actualizados los parches de seguridad de los correspondientes
+          navegadores). Como Usuario eres el único y exclusivo responsable del
+          uso por tu parte del Sitio Web y de los servicios y, consciente de
+          ello, aceptas voluntariamente tal responsabilidad.
+          <h5>En relación con los menores</h5>
+          En relación con la navegación en el Sitio Web por menores, a salvo de
+          lo indicado en los párrafos anteriores, los Usuarios quedáis
+          advertidos que la Sociedad no puede controlar, aparte de otras
+          circunstancias, que menores de edad hagan uso del Sitio Web y de sus
+          servicios. Por ello, además de no admitir el Sitio Web, la Sociedad ni
+          su Grupo empresarial responsabilidad alguna al respecto, se comunica
+          que serán los padres y tutores los únicos responsables de controlar y
+          asistir a los menores en la navegación por este Sitio Web y habilitar
+          cualesquiera otros mecanismos necesarios que, en su caso, impidan el
+          acceso por parte de los menores al Sitio Web y/o sus servicios, no
+          admitiendo la Sociedad ninguna reclamación al respecto.
+          <h5>Claves de acceso o similares</h5>
+          En caso de que algún servicio del Sitio Web incluya claves de acceso o
+          nombres de usuario, elegirás e indicarás tus propias claves de acceso
+          (nombre de usuario, contraseña, o similar), sin perjuicio de otros
+          datos que se puedan solicitar en el correspondiente formulario, y no
+          pudiendo elegir para ello palabras, expresiones o conjuntos
+          gráfico-denominativos malsonantes, injuriosos, coincidentes con
+          marcas, nombres comerciales, rótulos de establecimientos,
+          denominaciones sociales, expresiones publicitarias, nombres y
+          seudónimos de personajes de relevancia pública o famosos para cuya
+          utilización no esté autorizado y, en general, contrarios a la ley o a
+          las exigencias de la moral y buenas costumbres generalmente
+          aceptadas.<br /><br />
+          La asignación del nombre de usuario se produce de manera automática
+          previa elección del Usuario. En el supuesto de que estas claves
+          solicitadas se encuentren reservadas, deberás introducir unas nuevas
+          claves de acceso.<br /><br />
+          El Usuario se compromete a hacer un uso diligente de las claves de
+          acceso, a no ponerlas a disposición de terceros, y a comunicar a la
+          Sociedad la pérdida o robo de las claves de acceso y el posible acceso
+          por un tercero a las mismas.<br /><br />
+          Como Usuario eres el único y exclusivo responsable de tu
+          identificación de acceso al servicio. Las consecuencias derivadas de
+          su uso por terceros, su mal uso, pérdida u olvido del código secreto
+          de acceso de los usuarios - password - o bien del nombre de usuario
+          son de tu única y exclusiva responsabilidad.
+          <h5>Características generales de los servicios</h5>
+          Los servicios a los que puedes acceder en el Sitio Web son, por regla
+          general, gratuitos sin que los Usuarios tengáis que realizar
+          contraprestación alguna para poder disfrutar de ellos, salvo en lo
+          relativo al coste de conexión a través de la red de telecomunicaciones
+          suministrada por el proveedor de acceso que hubieras contratado. No
+          obstante lo anterior el acceso a determinados contenidos del Sitio Web
+          puede ser no gratuitos en función entre otros factores de volumen de
+          accesos por el Usuario o por el tipo de contenidos, de lo que, en todo
+          caso, se os informará y, en su caso, se exigirá vuestra suscripción o
+          registro.
+        </div>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
@@ -8,13 +119,112 @@ import { api } from "boot/axios";
 import { useQuasar } from "quasar";
 
 export default defineComponent({
-  name: "CondicionesUsoPage",
+  name: "PoliticaPrivPage",
   data() {
     return {};
   },
   setup() {},
-  methods: {},
+  methods: {
+    goBack() {},
+    goOtro() {
+      this.$router.push("/legal/politica-privacidad");
+    },
+  },
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.contenedor {
+  max-width: 1400px;
+  margin: 50px auto;
+  display: flex;
+  flex-direction: column;
+  padding: 0 25px;
+}
+
+.btn-addnew {
+  background-color: #05beed;
+  display: inline-block;
+  padding: 10px;
+  color: white;
+  margin: 25px 0;
+  cursor: pointer;
+  border-radius: 3px;
+  font-size: 1.1em;
+  transition: 0.2s ease;
+  margin-right: 10px;
+}
+
+.btn-addnew:hover {
+  background-color: #12ccfc;
+}
+
+.btn-container {
+  margin-bottom: 50px;
+}
+
+.title {
+  font-size: 3em;
+  position: relative;
+  margin-bottom: 50px;
+}
+
+.title::before {
+  content: "";
+  position: absolute;
+  width: 100px;
+  height: 2px;
+  background-color: black;
+  bottom: -20px;
+}
+
+.text {
+  font-size: 1.1em;
+}
+
+.m-header {
+  height: 75px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.m-header span {
+  margin-left: 50px;
+  font-size: 1.2em;
+}
+
+ul {
+  list-style-type: none;
+}
+
+ul li {
+  margin: 20px 0;
+  position: relative;
+}
+
+ul li::before {
+  content: "";
+  position: absolute;
+  width: 10px;
+  height: 10px;
+  background-color: #1976d2;
+  border-radius: 50%;
+  top: 6px;
+  left: -25px;
+}
+
+h5 {
+  border-bottom: 2px solid #1976d2;
+  padding-bottom: 10px;
+}
+
+.ver-otro {
+  color: #1976d2;
+  cursor: pointer;
+}
+
+.ver-otro:hover {
+  text-decoration: underline;
+}
+</style>
--- src/pages/PoliticaPriv.vue
+++ src/pages/PoliticaPriv.vue
@@ -1,11 +1,203 @@
 <template>
-  <q-page class="auth-container"> politica privacidad </q-page>
+  <q-page class="auth-container">
+    <q-header class="m-header"><span>E-Schools</span></q-header>
+    <div class="contenedor">
+      <div class="btn-container">
+        <div class="btn-addnew" @click="goBack">Volver atrás</div>
+      </div>
+      <div class="contenido">
+        <div class="title">Política de Privacidad</div>
+        <div class="text">
+          E-Schools te informa sobre su Política de Privacidad respecto del
+          tratamiento y protección de los datos de carácter personal de los
+          usuarios y clientes que puedan ser recabados por la navegación o
+          contratación de servicios a través del sitio Web E-Schools<br /><br />
+          En este sentido, el Titular garantiza el cumplimiento de la normativa
+          vigente en materia de protección de datos personales, reflejada en la
+          Ley Orgánica 3/2018, de 5 de diciembre, de Protección de Datos
+          Personales y de Garantía de Derechos Digitales (LOPD GDD). Cumple
+          también con el Reglamento (UE) 2016/679 del Parlamento Europeo y del
+          Consejo de 27 de abril de 2016 relativo a la protección de las
+          personas físicas (RGPD).<br /><br />
+          El uso de sitio Web implica la aceptación de esta Política de
+          Privacidad así como las condiciones incluidas en las condiciones de
+          uso
+          <span class="ver-otro" @click="goOtro">(ver condiciones de uso)</span
+          >.<br />
+          <h5>Principios aplicados en el tratamiento de datos</h5>
+          En el tratamiento de tus datos personales, el Titular aplicará los
+          siguientes principios que se ajustan a las exigencias del nuevo
+          reglamento europeo de protección de datos:
+          <ul>
+            <li>
+              <strong>Principio de licitud, lealtad y transparencia:</strong> El
+              Titular siempre requerirá el consentimiento para el tratamiento de
+              tus datos personales que puede ser para uno o varios fines
+              específicos sobre los que te informará previamente con absoluta
+              transparencia.
+            </li>
+            <li>
+              <strong>Principio de minimización de datos:</strong> El Titular te
+              solicitará solo los datos estrictamente necesarios para el fin o
+              los fines que los solicita.
+            </li>
+            <li>
+              <strong
+                >Principio de limitación del plazo de conservación:</strong
+              >
+              Los datos se mantendrán durante el tiempo estrictamente necesario
+              para el fin o los fines del tratamiento. El Titular te informará
+              del plazo de conservación correspondiente según la finalidad. En
+              el caso de suscripciones, el Titular revisará periódicamente las
+              listas y eliminará aquellos registros inactivos durante un tiempo
+              considerable.
+            </li>
+            <li>
+              <strong>Principio de integridad y confidencialidad:</strong> Tus
+              datos serán tratados de tal manera que su seguridad,
+              confidencialidad e integridad esté garantizada. Debes saber que el
+              Titular toma las precauciones necesarias para evitar el acceso no
+              autorizado o uso indebido de los datos de sus usuarios por parte
+              de terceros.
+            </li>
+          </ul>
+          <h5>Tus derechos</h5>
+          El Titular te informa que sobre tus datos personales tienes derecho a:
+          <ul>
+            <li>Solicitar el acceso a los datos almacenados.</li>
+            <li>Solicitar una rectificación o la cancelación.</li>
+            <li>Solicitar la limitación de su tratamiento.</li>
+            <li>Oponerte al tratamiento.</li>
+            <li>Solicitar la portabilidad de tus datos.</li>
+          </ul>
+          El ejercicio de estos derechos es personal y por tanto debe ser
+          ejercido directamente por el interesado, solicitándolo directamente al
+          Titular, lo que significa que cualquier cliente, suscriptor o
+          colaborador que haya facilitado sus datos en algún momento puede
+          dirigirse al Titular y pedir información sobre los datos que tiene
+          almacenados y cómo los ha obtenido, solicitar la rectificación de los
+          mismos, solicitar la portabilidad de sus datos personales, oponerse al
+          tratamiento, limitar su uso o solicitar la cancelación de esos datos
+          en los ficheros del Titular.<br /><br />
+          Para ejercitar tus derechos de acceso, rectificación, cancelación,
+          portabilidad y oposición tienes que enviar un correo electrónico a
+          E-Schools junto con la prueba válida en derecho como una fotocopia del
+          D.N.I. o equivalente.<br /><br />
+          Tienes derecho a la tutela judicial efectiva y a presentar una
+          reclamación ante la autoridad de control, en este caso, la Agencia
+          Española de Protección de Datos, si consideras que el tratamiento de
+          datos personales que te conciernen infringe el Reglamento.
+          <h5>Finalidad del tratamiento de datos personales</h5>
+          Cuando te conectas al sitio Web para mandar un correo al Titular, te
+          suscribes a su boletín o realizas una contratación, estás facilitando
+          información de carácter personal de la que el responsable es el
+          Titular. Esta información puede incluir datos de carácter personal
+          como pueden ser tu dirección IP, nombre y apellidos, dirección física,
+          dirección de correo electrónico, número de teléfono, y otra
+          información. Al facilitar esta información, das tu consentimiento para
+          que tu información sea recopilada, utilizada, gestionada y almacenada
+          por superadmin.es , sólo como se describe en el Aviso Legal y en la
+          presente Política de Privacidad.<br /><br />
+          Los datos personales y la finalidad del tratamiento por parte del
+          Titular es diferente según el sistema de captura de información:
+          <ul>
+            <li>
+              Para garantizar el cumplimiento de las condiciones recogidas en el
+              Aviso Legal y en la ley aplicable. Esto puede incluir el
+              desarrollo de herramientas y algoritmos que ayuden a este sitio
+              Web a garantizar la confidencialidad de los datos personales que
+              recoge.
+            </li>
+            <li>
+              Para apoyar y mejorar los servicios que ofrece este sitio Web.
+            </li>
+            <li>
+              Para analizar la navegación. El Titular recoge otros datos no
+              identificativos que se obtienen mediante el uso de cookies que se
+              descargan en tu ordenador cuando navegas por el sitio Web cuyas
+              caracterísiticas y finalidad están detalladas en la Política de
+              Cookies.
+            </li>
+            <li>
+              Para gestionar las redes sociales. el Titular tiene presencia en
+              redes sociales. Si te haces seguidor en las redes sociales del
+              Titular el tratamiento de los datos personales se regirá por este
+              apartado, así como por aquellas condiciones de uso, políticas de
+              privacidad y normativas de acceso que pertenezcan a la red social
+              que proceda en cada caso y que has aceptado previamente.
+            </li>
+          </ul>
+          El Titular tratará tus datos personales con la finalidad de
+          administrar correctamente su presencia en la red social, informarte de
+          sus actividades, productos o servicios, así como para cualquier otra
+          finalidad que las normativas de las redes sociales permitan.<br /><br />
+          En ningún caso el Titular utilizará los perfiles de seguidores en
+          redes sociales para enviar publicidad de manera individual.
+          <h5>Seguridad de los datos personales</h5>
+          Para proteger tus datos personales, el Titular toma todas las
+          precauciones razonables y sigue las mejores prácticas de la industria
+          para evitar su pérdida, mal uso, acceso indebido, divulgación,
+          alteración o destrucción de los mismos.<br /><br />
+          El sitio Web está alojado en "asdasd". La seguridad de tus datos está
+          garantizada, ya que toman todas las medidas de seguridad necesarias
+          para ello. Puedes consultar su política de privacidad para tener más
+          información.
+          <h5>Contenido de otros sitios web</h5>
+          Las páginas de este sitio Web pueden incluir contenido incrustado (por
+          ejemplo, vídeos, imágenes, artículos, etc.). El contenido incrustado
+          de otras web se comporta exactamente de la misma manera que si
+          hubieras visitado la otra web.<br /><br />
+          Estos sitios Web pueden recopilar datos sobre ti, utilizar cookies,
+          incrustar un código de seguimiento adicional de terceros, y supervisar
+          tu interacción usando este código.
+          <h5>Legitimación para el tratamiento de datos</h5>
+          La base legal para el tratamiento de tus datos es: el
+          consentimiento.<br /><br />
+          Para contactar con el Titular, suscribirte a un boletín o realizar
+          comentarios en este sitio Web tienes que aceptar la presente Política
+          de Privacidad.
+          <h5>Categorías de datos personales</h5>
+          Las categorías de datos personales que trata el Titular son:
+          <ul>
+            <li>Datos identificativos.</li>
+          </ul>
+          <h5>Conservación de datos personales</h5>
+          Los datos personales que proporciones al Titular se conservarán hasta
+          que solicites su supresión.
+          <h5>Exactitud y veracidad de los datos personales</h5>
+          Te comprometes a que los datos facilitados al Titular sean correctos,
+          completos, exactos y vigentes, así como a mantenerlos debidamente
+          actualizados.<br /><br />
+          Como Usuario del sitio Web eres el único responsable de la veracidad y
+          corrección de los datos que remitas al sitio exonerando a el Titular
+          de cualquier responsabilidad al respecto.
+          <h5>Aceptación y consentimiento</h5>
+          Como Usuario del sitio Web declaras haber sido informado de las
+          condiciones sobre protección de datos de carácter personal, aceptas y
+          consientes el tratamiento de los mismos por parte de el Titular en la
+          forma y para las finalidades indicadas en esta Política de Privacidad.
+          <h5>Revocabilidad</h5>
+          Para ejercitar tus derechos de acceso, rectificación, cancelación,
+          portabilidad y oposición tienes que enviar un correo electrónico a
+          E-Schools junto con la prueba válida en derecho como una fotocopia del
+          D.N.I. o equivalente.<br /><br />
+          El ejercicio de tus derechos no incluye ningún dato que el Titular
+          esté obligado a conservar con fines administrativos, legales o de
+          seguridad.
+          <h5>Cambios en la Política de Privacidad</h5>
+          El Titular se reserva el derecho a modificar la presente Política de
+          Privacidad para adaptarla a novedades legislativas o
+          jurisprudenciales, así como a prácticas de la industria.<br /><br />
+          Estas políticas estarán vigentes hasta que sean modificadas por otras
+          debidamente publicadas.
+        </div>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import { api } from "boot/axios";
-import { useQuasar } from "quasar";
 
 export default defineComponent({
   name: "PoliticaPrivPage",
@@ -13,8 +205,107 @@ export default defineComponent({
     return {};
   },
   setup() {},
-  methods: {},
+  methods: {
+    goBack() {},
+    goOtro() {
+      this.$router.push("/legal/condiciones-de-uso");
+    },
+  },
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.contenedor {
+  max-width: 1400px;
+  margin: 50px auto;
+  display: flex;
+  flex-direction: column;
+  padding: 0 25px;
+}
+
+.btn-addnew {
+  background-color: #05beed;
+  display: inline-block;
+  padding: 10px;
+  color: white;
+  margin: 25px 0;
+  cursor: pointer;
+  border-radius: 3px;
+  font-size: 1.1em;
+  transition: 0.2s ease;
+  margin-right: 10px;
+}
+
+.btn-addnew:hover {
+  background-color: #12ccfc;
+}
+
+.btn-container {
+  margin-bottom: 50px;
+}
+
+.title {
+  font-size: 3em;
+  position: relative;
+  margin-bottom: 50px;
+}
+
+.title::before {
+  content: "";
+  position: absolute;
+  width: 100px;
+  height: 2px;
+  background-color: black;
+  bottom: -20px;
+}
+
+.text {
+  font-size: 1.1em;
+}
+
+.m-header {
+  height: 75px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.m-header span {
+  margin-left: 50px;
+  font-size: 1.2em;
+}
+
+ul {
+  list-style-type: none;
+}
+
+ul li {
+  margin: 20px 0;
+  position: relative;
+}
+
+ul li::before {
+  content: "";
+  position: absolute;
+  width: 10px;
+  height: 10px;
+  background-color: #1976d2;
+  border-radius: 50%;
+  top: 6px;
+  left: -25px;
+}
+
+h5 {
+  border-bottom: 2px solid #1976d2;
+  padding-bottom: 10px;
+}
+
+.ver-otro {
+  color: #1976d2;
+  cursor: pointer;
+}
+
+.ver-otro:hover {
+  text-decoration: underline;
+}
+</style>
